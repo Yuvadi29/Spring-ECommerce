f9fd6863c02df5d9b1c9109f48b0152eabf5fbac
Yuvadi29 <adityatrivedi612@gmail.com>
2024-03-02 22:07:33 +0530

    Initial Commit and Spring Setup

---
+++ src/test/java/com/example/server/Shukra/ShukraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server.Shukra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShukraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
